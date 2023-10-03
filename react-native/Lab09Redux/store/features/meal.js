715dc107e9bd094c0ed7190e0d9a983db02e33b8
Geardevop <gearkiskeedev@gmail.com>
2023-10-03 11:41:12 +0700

    add lab9 lab10

---
+++ react-native/Lab09Redux/store/features/meal.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {MEALS} from "../../data/dummy-data.js"
+
+export const mealSlice = createSlice({
+    name: 'meals',
+    initialState: {
+        meals: MEALS,
+        filteredMeals: MEALS,
+        favoriteMeals: [],
+    },
+    reducers: {
+        toggleFavorite: (state, action) => {
+            // console.log("payload na: "+action.payload.id);
+            console.log("Fav na: " + state.favoriteMeals);
+            const isInFav =  state.favoriteMeals.findIndex(meal =>  meal.id === action.payload);
+
+            if (isInFav >= 0) {
+                //remove from favorite
+                const updateStateFav =  state.favoriteMeals.filter(meal => meal.id !== action.payload);
+                return { ...state, favoriteMeals: updateStateFav };
+                // return state;
+            } else {
+                //add to favorite
+                // console.log("Item"+ MEALS, "ID"+ action.payload);
+                
+                const meal = state.meals.find(meal => meal.id === action.payload);
+                const updateFav =  [...state.favoriteMeals, meal]
+                return { ...state, favoriteMeals: updateFav };
+                // console.log("Fav na: " + state.favoriteMeals));
+              
+            }
+        }
+    }
+});
+
+export const { toggleFavorite } = mealSlice.actions;
+export default mealSlice.reducer;
